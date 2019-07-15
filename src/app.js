a7b7c5e89e9cd4df07cfdaae14569122d46b334b
Akshay Joshi <32253592+akshay14021@users.noreply.github.com>
2019-07-14 20:24:32 -0400

    QoL

--- src/app.js
+++ src/app.js
@@ -17,80 +17,80 @@ hbs.registerPartials(path.join(__dirname, '../templates/partials'))
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Akshay Joshi'
-    })
+	res.render('index', {
+		title: 'Weather',
+		name: 'Akshay Joshi'
+	})
 })
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Akshay Joshi'
-    })
+	res.render('about', {
+		title: 'About Me',
+		name: 'Akshay Joshi'
+	})
 })
 
 app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help',
-        helpText: 'Go to weather tab and search the location.',
-        name: 'Akshay Joshi'
-    })
+	res.render('help', {
+		title: 'Help',
+		helpText: 'Go to weather tab and search the location.',
+		name: 'Akshay Joshi'
+	})
 })
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'Please provide address.'
-        })
-    }
-    geocode(req.query.address, (error, data) => {
-        if (error) {
-            return res.send({
-                error: error
-            })
-        }
-        const longitude = data.center[0]
-        const latitude = data.center[1]
-        const location = data.place_name
+	if (!req.query.address) {
+		return res.send({
+			error: 'Please provide address.'
+		})
+	}
+	geocode(req.query.address, (error, data) => {
+		if (error) {
+			return res.send({
+				error: error
+			})
+		}
+		const longitude = data.center[0]
+		const latitude = data.center[1]
+		const location = data.place_name
 
-        weather(latitude, longitude, (error, data) => {
-            if (error) {
-                return res.send({
-                    error: error
-                })
-            }
-            const summary = data.daily.data[0].summary
-            const temperature = data.currently.temperature
-            const precip = data.currently.precipProbability
+		weather(latitude, longitude, (error, data) => {
+			if (error) {
+				return res.send({
+					error: error
+				})
+			}
+			const summary = data.daily.data[0].summary
+			const temperature = data.currently.temperature
+			const precip = data.currently.precipProbability
 
-            return res.send({
-                location: location,
-                forecast: `${summary} It is currently ${temperature} degrees out. There is ${precip} % chance of rain`,
-                address: req.query.address
-            })
-        })
-    })
+			return res.send({
+				location: location,
+				forecast: `${summary} It is currently ${temperature} degrees out. There is ${precip} % chance of rain`,
+				address: req.query.address
+			})
+		})
+	})
 })
 
 app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        errorMessage: 'Help article not found',
-        name: 'Akshay Joshi',
-    })
+	res.render('404', {
+		title: '404',
+		errorMessage: 'Help article not found',
+		name: 'Akshay Joshi'
+	})
 })
 
 app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        errorMessage: 'Page not found',
-        name: 'Akshay Joshi'
-    })
+	res.render('404', {
+		title: '404',
+		errorMessage: 'Page not found',
+		name: 'Akshay Joshi'
+	})
 })
 
 // Server Setup
 
 app.listen(port, () => {
-    console.log('Server is up. Happy coding!')
+	console.log('Server is up. Happy coding!')
 })
